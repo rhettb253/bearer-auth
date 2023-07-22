c5f718f605ba0e26fe6a9f6a0e2f5a05e2990306
Rhett Beardemphl <rhettbeardemphl@hotmail.com>
2023-07-22 15:03:59 -0700

    got code situated w no tests passing ha

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 'use strict';
 
+require('dotenv').config();
+let { startup } = require('./src/server');
+
+// Start up DB Server
+const { db } = require('./src/auth/models/index.js');
+db.sync()
+  .then(() => startup(process.env.PORT))
+  .catch((e) => console.error('Wasn\'t able to start server: ' + e.message || e));
